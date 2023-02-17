7906c6ae664789a249b0bc2a90c793f1d71acc1c
BashiruEnglish <englishbashiru@gmail.com>
2023-02-17 02:03:03 -0800

    all alphabets but

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char d;
+
+	char e;
+
+	d = 'a';
+	e = 'Z';
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}
+	while
+		(e <= 'Z') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
+
