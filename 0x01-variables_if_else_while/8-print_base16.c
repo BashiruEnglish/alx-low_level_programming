8a39c1ab1689691f35d96154d7cbd4b29689b497
BashiruEnglish <englishbashiru@gmail.com>
2023-02-17 03:06:42 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char k;
+
+	int l;
+
+	k = 'a';
+	l = 0;
+	while
+		(l < 10) {
+			putchar(l + '0');
+			l++;
+		}
+	while
+		(k <= 'f') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
+
