4d80cccc212256f8d3d59153404209da427654d8
BashiruEnglish <englishbashiru@gmail.com>
2023-02-21 10:39:10 -0800

    addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @b: first integer
+ * @e: second integer
+ * Return: summation of both
+ */
+
+int add(int b, int e)
+{
+	return (b + e);
+}
+
