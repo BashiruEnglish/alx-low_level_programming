1eb9fbb20fbcc54895beaadf8ef947cd0c2f7225
BashiruEnglish <englishbashiru@gmail.com>
2023-02-21 01:22:13 -0800

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+		_putchar(lower);
+
+	_putchar('\n');
+}
+
