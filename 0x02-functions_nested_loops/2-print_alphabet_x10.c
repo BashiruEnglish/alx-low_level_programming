697eaacbea00ff102da6333943e8d0bd8a94a8b7
BashiruEnglish <englishbashiru@gmail.com>
2023-02-21 01:27:57 -0800

    x10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabtes 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char lower;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (lower = 'a'; lower <= 'z'; lower++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
+
