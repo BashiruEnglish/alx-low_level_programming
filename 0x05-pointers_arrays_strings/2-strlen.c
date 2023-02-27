42c4e42a00c72587d060e92a71f2757061da8ff7
BashiruEnglish <englishbashiru@gmail.com>
2023-02-27 00:31:41 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,8 +3,8 @@
 
 /**
  * _strlen - Returns the length of a string
- * @str: The string we want the length of
- * Return: the length of our string @str.
+ * @s: The string we want the length of
+ * Return: the length of our string @s.
  */
 int _strlen(char *s)
 {
